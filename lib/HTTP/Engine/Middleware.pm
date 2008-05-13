eb8c77cddfbef48f885f9901b5f49557f0e0f1a9
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-05-13 07:54:55 +0000

    initial import

---
+++ lib/HTTP/Engine/Middleware.pm
@@ -0,0 +1,39 @@
+package HTTP::Engine::Middleware;
+use strict;
+use warnings;
+use 5.00800;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+HTTP::Engine::Middleware -
+
+=head1 SYNOPSIS
+
+  use HTTP::Engine::Middleware;
+
+=head1 DESCRIPTION
+
+HTTP::Engine::Middleware is
+
+=head1 TODO
+
+no plan :-)
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
