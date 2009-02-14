e141cc4bd74cfb446a601351881ffe6affddb746
yappo <yappo@d0d07461-0603-4401-acd4-de1884942a52>
2009-02-14 06:04:35 +0000

    Middleware::Static add docs for has multi document root config

--- lib/HTTP/Engine/Middleware/Static.pm
+++ lib/HTTP/Engine/Middleware/Static.pm
@@ -95,6 +95,35 @@ HTTP::Engine::Middleware::Profile - handler for static files
     # $ GET http//localhost/robots.txt
     # to get the /path/to/htdocs/robots.txt
 
+has multi document root
+
+    my $mw = HTTP::Engine::Middleware->new;
+    $mw->install(
+        'HTTP::Engine::Middleware::Static' => {
+            regexp  => qr{^/(robots.txt|css/.+)$},
+            docroot => '/path/to/htdocs/',
+        },
+        'HTTP::Engine::Middleware::Static' => {
+            regexp  => qr{^/foo(/.+)$},
+            docroot => '/foo/bar/',
+        },
+    );
+    HTTP::Engine->new(
+        interface => {
+            module => 'YourFavoriteInterfaceHere',
+            request_handler => $mw->handler( \&handler ),
+        }
+    )->run();
+
+    # $ GET http//localhost/css/foo.css
+    # to get the /path/to/htdocs/css/foo.css
+
+    # $ GET http//localhost/robots.txt
+    # to get the /path/to/htdocs/robots.txt
+
+    # $ GET http//localhost/foo/baz.html
+    # to get the /foo/bar/baz.txt
+
 
 =head1 DESCRIPTION
 
