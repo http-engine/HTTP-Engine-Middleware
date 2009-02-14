7a50fe61088569daa27f00724ce3291b62265aa4
yappo <yappo@d0d07461-0603-4401-acd4-de1884942a52>
2009-02-14 06:25:34 +0000

    update SYNOPSIS

--- lib/HTTP/Engine/Middleware/Static.pm
+++ lib/HTTP/Engine/Middleware/Static.pm
@@ -79,7 +79,7 @@ HTTP::Engine::Middleware::Profile - handler for static files
 
     my $mw = HTTP::Engine::Middleware->new;
     $mw->install( 'HTTP::Engine::Middleware::Static' => {
-        regexp  => qr{^/(robots.txt|css/.+)$},
+        regexp  => qr{^/(robots.txt|favicon.ico|(?:css|js|img)/.+)$},
         docroot => '/path/to/htdocs/',
     });
     HTTP::Engine->new(
@@ -92,6 +92,9 @@ HTTP::Engine::Middleware::Profile - handler for static files
     # $ GET http//localhost/css/foo.css
     # to get the /path/to/htdocs/css/foo.css
 
+    # $ GET http//localhost/js/jquery.js
+    # to get the /path/to/htdocs/js/jquery.js
+
     # $ GET http//localhost/robots.txt
     # to get the /path/to/htdocs/robots.txt
 
@@ -100,7 +103,7 @@ has multi document root
     my $mw = HTTP::Engine::Middleware->new;
     $mw->install(
         'HTTP::Engine::Middleware::Static' => {
-            regexp  => qr{^/(robots.txt|css/.+)$},
+            regexp  => qr{^/(robots.txt|favicon.ico|(?:css|js|img)/.+)$},
             docroot => '/path/to/htdocs/',
         },
         'HTTP::Engine::Middleware::Static' => {
@@ -123,6 +126,26 @@ has multi document root
 
     # $ GET http//localhost/foo/baz.html
     # to get the /foo/bar/baz.txt
+
+through only the specific URL to backend
+
+    my $mw = HTTP::Engine::Middleware->new;
+    $mw->install( 'HTTP::Engine::Middleware::Static' => {
+        regexp  => qr{^/(robots.txt|favicon.ico|(?:css|img)/.+|js/(?!dynamic).+)$},
+        docroot => '/path/to/htdocs/',
+    });
+    HTTP::Engine->new(
+        interface => {
+            module => 'YourFavoriteInterfaceHere',
+            request_handler => $mw->handler( \&handler ),
+        }
+    )->run();
+
+    # $ GET http//localhost/js/jquery.js
+    # to get the /path/to/htdocs/js/jquery.js
+
+    # $ GET http//localhost/js/dynamic-json.js
+    # to get the your application response
 
 
 =head1 DESCRIPTION
