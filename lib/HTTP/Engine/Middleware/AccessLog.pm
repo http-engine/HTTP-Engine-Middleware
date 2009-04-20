3598100a52fb1e2b3ec232b53c1f9104df958cd8
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2009-04-20 08:32:10 +0000

    added Middleware::AccessLog.

---
+++ lib/HTTP/Engine/Middleware/AccessLog.pm
@@ -0,0 +1,101 @@
+package HTTP::Engine::Middleware::AccessLog;
+use HTTP::Engine::Middleware;
+use Carp ();
+use DateTime;
+
+with 'HTTP::Engine::Middleware::Role::Logger';
+
+has format => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => '%h %l %u %t "%r" %>s %b "%{Referer}i" "%{User-agent}i"',
+);
+
+after_handle {
+    my ( $c, $self, $req, $res ) = @_;
+
+    my $msg = $self->format;
+
+    $msg =~ s/\%\{([\w-]+)\}i/$req->header($1) || '-'/ge;                   # %{User-Agent}
+    $msg =~ s/\%(?:[><])?([a-z])/handle_char($req, $res, $1) || '-'/ge;    # %r
+    $msg =~ s/\%\%/%/g;                                           # %%
+
+    $self->log($msg);
+
+    return $res;
+};
+
+sub handle_char {
+    my ($req, $res, $char) = @_;
+    my $code = +{
+        'h' => sub {
+            $req->address, # remote host
+        },
+        'l' => sub {
+            '-', # remote log name
+        },
+        'u' => sub {
+            $req->user; # user name
+        },
+        't' => sub {
+            my $dt = DateTime->now;
+            $dt->strftime("[%d/%b/%y:%H:%M:%S %z]");
+        },
+        'r' => sub {
+            join ' ', $req->method, $req->uri->path, ($req->protocol||'HTTP/1.0'); # ?
+        },
+        'b' => sub {
+            $res->content_length || '-'; # size of response in bytes, excluding HTTP headers
+        },
+        's' => sub {
+            $res->status
+        },
+    }->{$char};
+    if ($code) {
+        return $code->();
+    } else {
+        Carp::croak "unknown log char '$char'";
+    }
+}
+
+__MIDDLEWARE__
+
+__END__
+
+=head1 NAME
+
+HTTP::Engine::Middleware::AccessLog - write access log
+
+=head1 SYNOPSIS
+
+    my $mw = HTTP::Engine::Middleware->new;
+    $mw->install( 'HTTP::Engine::Middleware::AccessLog' => {
+        logger => sub {
+            warn @_; # your own callback routine
+        },
+        format => '%h %l %u %t "%r" %>s %b "%{Referer}i" "%{User-agent}i"',
+    });
+    HTTP::Engine->new(
+        interface => {
+            module => 'YourFavoriteInterfaceHere',
+            request_handler => $mw->handler( \&handler ),
+        }
+    )->run();
+
+=head1 DESCRIPTION
+
+This middleware prints access log like apache.
+
+This module's log format string is a subset of Apache.
+If you want to use more syntax, patches welcome :)
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno
+
+=head1 SEE ALSO
+
+L<HTTP::Engine::Middleware>
+L<http://httpd.apache.org/docs/2.0/en/mod/mod_log_config.html>
+
+
---
+++ t/200_middlewares/accesslog.t
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+use Test::More;
+
+plan skip_all => 'this middleware requires DateTime' unless eval 'use DateTime; 1;';
+plan tests => 3;
+
+use HTTP::Engine;
+use HTTP::Engine::Middleware;
+use HTTP::Engine::Response;
+use HTTP::Request;
+use HTTP::Request::Common;
+use Time::Piece;
+
+my $mw = HTTP::Engine::Middleware->new;
+$mw->install(
+    'HTTP::Engine::Middleware::AccessLog',
+    {   logger => sub {
+            my ( $message ) = @_;
+            ::like $message, qr{127.0.0.1 - - \[\d\d/\w+/\d\d:\d\d:\d\d:\d\d \+0000\] "POST / HTTP/1.0" 200 - "http://mixi.jp/" "internatoexplolerr"};
+        }
+    }
+);
+
+my $request
+    = HTTP::Request->new( 'POST' => 'http://localhost/?getparam=1', HTTP::Headers->new(
+        'User-Agent' => 'internatoexplolerr',
+        Referer => 'http://mixi.jp/',
+        'Content-Length' => 0,
+        'content-type' => 'text/plain',
+    ));
+my $res = HTTP::Engine->new(
+    interface => {
+        module          => 'Test',
+        request_handler => $mw->handler(
+            sub { HTTP::Engine::Response->new( body => 'ok' ) }
+        ),
+    },
+)->run($request);
+my $out = $res->content;
+
+is $res->code, '200', 'response code';
+is $out, 'ok', 'response content';
+
